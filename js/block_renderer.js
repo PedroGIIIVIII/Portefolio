61dd80b2536744c220652208a3e227879218e9ff
ToyaHinata <shmeerzcomercial@gmail.com>
2026-02-13 00:43:57 +0000

    Update

--- js/block_renderer.js
+++ js/block_renderer.js
@@ -30,28 +30,6 @@ export function InitSkills() {
     <h1 data-translations="on">Skills</h1>
     <div class="skill_category">
             <h2>Programming Related</h2>
-
-        <div class="skill">
-            <div class="skill_header">
-                <div class="skill_left">
-                    <div class="skill_meter">
-                        <span class="dot active"></span>
-                        <span class="dot active"></span>
-                        <span class="dot"></span>
-                        <span class="dot"></span>
-                    </div>
-
-                    <div class="skill_title">
-                        <img class="skill_icon" src="../assets/cpp_icon.png">
-                        <span class="skill_name">C++</span>
-                    </div>
-                    <p class="skill_desc">
-                    |   Basic Memory Managnment, Unreal Engine´s C++, Macros, Basic Gameplay Systems, Polymorphism
-                    </p>
-                </div>
-                <span class="skill_level can_do">Can Do</span>
-            </div>
-        </div>
         <div class="skill">
             <div class="skill_header">
                 <div class="skill_left">
@@ -72,6 +50,27 @@ export function InitSkills() {
                 </div>
                 <span class="skill_level comfortable">Comfortable</span>
                 </div>
+        </div>
+        <div class="skill">
+            <div class="skill_header">
+                <div class="skill_left">
+                    <div class="skill_meter">
+                        <span class="dot active"></span>
+                        <span class="dot active"></span>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                    </div>
+
+                    <div class="skill_title">
+                        <img class="skill_icon" src="../assets/cpp_icon.png">
+                        <span class="skill_name">C++</span>
+                    </div>
+                    <p class="skill_desc">
+                    |   Basic Memory Managnment, Unreal Engine´s C++, Macros, Basic Gameplay Systems, Polymorphism
+                    </p>
+                </div>
+                <span class="skill_level can_do">Can Do</span>
+            </div>
         </div>
         <div class="skill">
             <div class="skill_header">
@@ -138,52 +137,8 @@ export function InitSkills() {
         </div>
     </div>
     <div class="skill_category">
-            <h2>3D Related</h2>
-        <div class="skill">
-            <div class="skill_header">
-                <div class="skill_left">
-                    <div class="skill_meter">
-                        <span class="dot active"></span>
-                        <span class="dot active"></span>
-                        <span class="dot active"></span>
-                        <span class="dot"></span>
-                    </div>
-
-                    <div class="skill_title">
-                        <img class="skill_icon" src="../assets/blender_icon.png">
-                        <span class="skill_name">Blender</span>
-                    </div>
-                    <p class="skill_desc">
-                    |   Character Creation, Environment Asset Creation, Rigging, Texturing, Material Creation, UV Unwrapping
-                    </p>
-                </div>
-                <span class="skill_level comfortable">Comfortable</span>
-                </div>
-        </div>
-
-        <div class="skill">
-            <div class="skill_header">
-                <div class="skill_left">
-                    <div class="skill_meter">
-                        <span class="dot active"></span>
-                        <span class="dot"></span>
-                        <span class="dot"></span>
-                        <span class="dot"></span>
-                    </div>
-
-                    <div class="skill_title">
-                        <img class="skill_icon" src="../assets/3ds_icon.png">
-                        <span class="skill_name">3DS Max</span>
-                    </div>
-                    <p class="skill_desc">
-                    |   Basic 3D Modelling
-                    </p>
-                </div>
-                <span class="skill_level experimental">Experimental</span>
-                </div>
-        </div>
-
-        <div class="skill">
+        <h2>3D Related</h2>
+         <div class="skill">
             <div class="skill_header">
                 <div class="skill_left">
                     <div class="skill_meter">
@@ -210,6 +165,27 @@ export function InitSkills() {
                     <div class="skill_meter">
                         <span class="dot active"></span>
                         <span class="dot active"></span>
+                        <span class="dot active"></span>
+                        <span class="dot"></span>
+                    </div>
+
+                    <div class="skill_title">
+                        <img class="skill_icon" src="../assets/blender_icon.png">
+                        <span class="skill_name">Blender</span>
+                    </div>
+                    <p class="skill_desc">
+                    |   Character Creation, Environment Asset Creation, Rigging, Texturing, Material Creation, UV Unwrapping
+                    </p>
+                </div>
+                <span class="skill_level comfortable">Comfortable</span>
+                </div>
+        </div>
+        <div class="skill">
+            <div class="skill_header">
+                <div class="skill_left">
+                    <div class="skill_meter">
+                        <span class="dot active"></span>
+                        <span class="dot active"></span>
                         <span class="dot"></span>
                         <span class="dot"></span>
                     </div>
@@ -225,6 +201,27 @@ export function InitSkills() {
                 <span class="skill_level can_do">Can Do</span>
                 </div>
         </div>
+        <div class="skill">
+            <div class="skill_header">
+                <div class="skill_left">
+                    <div class="skill_meter">
+                        <span class="dot active"></span>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                        <span class="dot"></span>
+                    </div>
+
+                    <div class="skill_title">
+                        <img class="skill_icon" src="../assets/3ds_icon.png">
+                        <span class="skill_name">3DS Max</span>
+                    </div>
+                    <p class="skill_desc">
+                    |   Basic 3D Modelling
+                    </p>
+                </div>
+                <span class="skill_level experimental">Experimental</span>
+                </div>
+        </div>
     </div>
     `;
 }
@@ -234,19 +231,20 @@ export function InitProjects() {
     container.innerHTML = 
     `
         <h1 data-translations="projects">Projects</h1>
-        <div class="projects_container" id="projects_list"></div>
-        <div id="project_modal" class="project_modal"></div>
+        <div class="projects_container" id="projects_list">
+        </div>
+        <div id="project_modal" class="project_modal">
+        </div>
     `;
 
     const projectslist = document.getElementById('projects_list');
     
         PROJECTS.forEach(projects=> {
-        const statuslabel = projects.status.charAt(0).toUpperCase() + projects.status.slice(1);
         const card = document.createElement('div');
         card.className = 'game_card';
         card.innerHTML = `
-            <div class="status ${projects.status}"><span>${statuslabel}</span></div>
-            <img src="${projects.images[0]}" class="game_image" alt="${projects.title}">
+            <div class="status ${projects.status}"><span>${projects.status}</span></div>
+            <img src="${projects.images}" class="game_image" alt="${projects.title}">
             <div class="game_info">
                 <h2 class="game_title">${projects.title}</h2>
                 <p class="game_genre">${projects.genre}</p>
@@ -315,50 +313,107 @@ export function ShowProjectDetail(PROJECTS) { //Modular DIV for display of proje
 
     modal.classList.add('active');
     
-    const close = modal.querySelector('.modal_close');
-    const overlay = modal.querySelector('.modal_overlay');
-    
-    close.addEventListener('click', CloseProjectDetail);
-    overlay.addEventListener('click', CloseProjectDetail);
+    modal.querySelector('.modal_close').addEventListener('click', CloseProjectDetail);
     
     document.addEventListener('keydown', (e) => {
         if (e.key === 'Escape') CloseProjectDetail();
     });
 }
 
-export function ShowModelDetail(MODELS) { //Modular DIV for display of models
-    const modal = document.getElementsByClassName('models_modal');
+export function ShowModelDetails(m)
+{
+    console.log("Opened Context Window")
 
-    MODELS.forEach(models => {
-        
-    })
+    const modal = document.querySelector('.models_modal');
 
+    const softwarelist = m.software.map(soft => `<span class="tech_badge">${soft}</span>`).join('');
 
     modal.innerHTML = `
         <div class="modal_overlay"></div>
+
         <div class="models_modal_content">
             <button class="modal_close" aria-label="Close">×</button>
-            <h2>${MODELS.name}</h2>
+
+            <div class="modal_header">
+                <h1>${m.name}</h1>
+            </div>
+
+            <div class="modal_body">
+                <div class="modal_gallery">
+                    <img src="${m.img}" 
+                    alt="${m.name}" 
+                    class="modal_main_image">
+                </div>
+
+                <div class="modal_info">
+
+                    <section class="info_section">
+                        <h3>Description</h3>
+                        <p>${m.description}</p>
+                    </section>
+
+                    <section class="info_section">
+                        <h3>Software Used</h3>
+                        <div class="tech_container">
+                            ${softwarelist}
+                        </div>
+                    </section>
+
+                    <section class="info_section">
+                        <h3>Technical Info</h3>
+                        <p>Polygon Count: ${m.polygoncount}</p>
+                    </section>
+
+                </div>
+            </div>
         </div>
     `;
+
     modal.classList.add('active');
+    modal.querySelector('.modal_close').addEventListener('click', CloseModelDetail);
+}
+
+export function InitModels() {
+    const cards = document.querySelectorAll('.work_card');
+
+    cards.forEach(card => {
+
+        const id = card.dataset.id;
+        const m_d = MODELS.find(x => x.id === id);
+        console.log("Log model")
+        if (!m_d)
+        {
+            return;
+        }
+
+        card.addEventListener('click', () => {
+            ShowModelDetails(m_d);
+        });
+
+    });
 }
 
 export function CloseProjectDetail() {
     const modal = document.getElementById('project_modal');
     modal.classList.remove('active');
-    setTimeout(() => {
-        modal.innerHTML = '';
-    }, 300);
+    setTimeout(() => {modal.innerHTML = '';}, 300);
+}
+
+export function CloseModelDetail() {
+    const modal = document.querySelector('.models_modal');
+    modal.classList.remove('active');
+    setTimeout(() => { modal.innerHTML = ''; }, 300);
 }
 
 export function InitGoals() {
     const container = document.querySelector('.aboutme_content');
     container.innerHTML = 
     `
-        <h1 data-translations="goals">${GetTranslationFromKey('goals')}</h1>
+        <h1 data-translations="goals">Goals</h1>
         <div>
             <h2>Something is being worked on...</h2>
         </div>
     `;
 }
+
+window.InitModels = InitModels;
--- js/constants.js
+++ js/constants.js
@@ -24,7 +24,7 @@ export const PROJECTS =
         fulldescription: 'Huddle is a third-person horror game focused on avoiding a murderer seal that plans on eating you alive. My Roles were as 3D Artist and Environment Artist, with that, i was responsible for crafting the visual identity of the game world from character models to the environments.',
         technologies: ['Unity', 'Blender', 'Fmod'],
         teamsize: '5 people',
-        images: ['../assets/placeholder_banner.png', '../assets/placeholder_banner.png'],
+        images: ['../assets/placeholder_banner.png'],
         links: { github: 'https://github.com/PedroGIIIVIII/Huddle'}
     },
     {
@@ -38,7 +38,7 @@ export const PROJECTS =
         fulldescription: 'GeoZoo is an edutainment project designed to make learning animals and its associated environments engaging and fun. I performed the role of 2D Animator which involves creating smooth, captivating animations for the animals.',
         technologies: ['Unity', 'Photoshop', 'Adobe After Effects', 'Adobe Illustrator'],
         teamsize: '5 people',
-        images: ['../assets/placeholder_banner.png', '../assets/placeholder_banner.png'],
+        images: ['../assets/placeholder_banner.png'],
         links: { github: '#',}
     },
     {
@@ -52,7 +52,7 @@ export const PROJECTS =
         fulldescription: 'Project Blue Glow is a solo development effort where I handle all aspects of the development of the game while making a FPS combined with psychological thriller elements with it being currently halted to focus on other projects.',
         technologies: ['Unity', 'C#'],
         teamsize: '1 (Solo)',
-        images: ['../assets/placeholder_banner.png', '../assets/placeholder_banner.png'],
+        images: ['../assets/placeholder_banner.png'],
         links: { github: '#',}
     },
     {
@@ -66,7 +66,7 @@ export const PROJECTS =
         fulldescription: 'FrutigerSpace Wiki was a web-based project aimed at creating a comprehensive wiki for a gaming community as part of the community of the game FrutigerSpace. Unfortunaly, the project was canceled to pursue other opportunities but served as valuable experience in web development and the architecture.',
         technologies: ['HTML', 'PHP', 'CSS'],
         teamsize: '2 people',
-        images: ['../assets/placeholder_banner.png', '../assets/placeholder_banner.png'],
+        images: ['../assets/placeholder_banner.png'],
         links: { github: '#',}
     }
 ];
@@ -75,6 +75,32 @@ export const MODELS =
 [
     {
         id: 'seal_huddle',
-        name: '#',
+        name: 'Seal Model',
+        category: 'enemies',
+        description: 'Stylized low-poly model of a seal for the videogame Huddle.',
+        software: ['Blender'],
+        polygoncount: 'X quads',
+        img: ['../assets/placeholder_banner.png'],
+        projectlink: '#'
     },
+    {
+        id: 'penguin',
+        name: 'Penguin Model',
+        category: 'enemies',
+        description: 'Stylized Low-Poly Model of the main protagonist for the videogame Huddle.',
+        software: ['Blender'],
+        polygoncount: 'X quads',
+        img: ['../assets/placeholder_banner.png'],
+        projectlink: '#'
+    },
+    {
+        id: 'spacecraft',
+        name: 'Spacecraft Model',
+        category: 'enemies',
+        description: 'Used a everyday lamp and transformed into a spaceship',
+        software: ['Blender', 'Substance Painter'],
+        polygoncount: 'X quads',
+        img: ['../assets/placeholder_banner.png'],
+        projectlink: '#'
+    }
 ] 
